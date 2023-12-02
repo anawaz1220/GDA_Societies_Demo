2c9ffd91a9c9b51c2735c93883f936e33a1c21a8
Asif Nawaz <asifnawaz1220@gmail.com>
2023-12-02 07:57:56 +0500

    Add files via upload

---
+++ precache-manifest.b6f7df5dd28643be73514a8f8c4eee30.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a513f287cf3cf242906676fac29e34c",
+    "url": "Critical.jpg"
+  },
+  {
+    "revision": "d496f05c09b10abb15ea858a7abe2dcc",
+    "url": "GDA_Logo.png"
+  },
+  {
+    "revision": "1ce4ebb0401098c45829d41e329d03a8",
+    "url": "Normal.png"
+  },
+  {
+    "revision": "c75248eeb550b35ff370356537e3c1f9",
+    "url": "Screenshot_11 copy copy.png"
+  },
+  {
+    "revision": "3c98970b4f41a26741570780e4dd5d25",
+    "url": "Shutdown.png"
+  },
+  {
+    "revision": "04e154e9d7ea293783fbcb13fbb6e74b",
+    "url": "Society.jpg"
+  },
+  {
+    "revision": "88b86f8b88d326012aef59e994a2593d",
+    "url": "Society_1.jpg"
+  },
+  {
+    "revision": "7b084d84b8cd3e2cf53b4429ce1f2ec2",
+    "url": "Zong.png"
+  },
+  {
+    "revision": "201c7262b903ea82a89a",
+    "url": "css/app.d0dc93a6.css"
+  },
+  {
+    "revision": "9a2faaed76cb66292862",
+    "url": "css/chunk-vendors.9590b94d.css"
+  },
+  {
+    "revision": "0365d04f13747ed74eea8c348d0eeb56",
+    "url": "globiz.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "34ba58b05b7d199f415fd71c49dc63ac",
+    "url": "img/tower_icon.png"
+  },
+  {
+    "revision": "229a374ee65a1b39bb4a3169e70f7434",
+    "url": "index.html"
+  },
+  {
+    "revision": "e712967fa1e416727695",
+    "url": "js/about.b47a456a.js"
+  },
+  {
+    "revision": "201c7262b903ea82a89a",
+    "url": "js/app.c7da4766.js"
+  },
+  {
+    "revision": "9a2faaed76cb66292862",
+    "url": "js/chunk-vendors.845ea39f.js"
+  },
+  {
+    "revision": "d246407ac4d230b530feaa3aa565ce55",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8",
+    "url": "marker-icon.png"
+  },
+  {
+    "revision": "3a69a461549b182809541ba3a63725a2",
+    "url": "print.jpg"
+  },
+  {
+    "revision": "11b6b3bd8ff05d1fe2ccf4f5ac74d3df",
+    "url": "red.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "6912d8da0253ac07578d0b316fe681d7",
+    "url": "yellow.png"
+  }
+]);
